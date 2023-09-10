1366f263ab89ea3abc518c05a5258bca3fbe1aff
inasekin <nasekinid8591@yandex.ru>
2023-09-10 16:54:36 +0300

    fix: fix eslint errors

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-import path from "path";
-import fs from "fs";
+import path from 'path';
+import fs from 'fs';
 
 export const getFilePath = (fileName) => path.resolve(process.cwd(), fileName);
 export const readFile = (fileName) => fs.readFileSync(getFilePath(fileName), 'utf8');
